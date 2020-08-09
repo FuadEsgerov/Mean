133442c4e4246b90e318b6210d64015006419cb3
FuadEsgerov <fuadra@code.edu.az>
2020-08-09 17:42:27 +0400

    Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button'
-import {MatCardModule} from '@angular/material/card'
+import {DemoMaterialModule} from '../app/material-module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule
+    DemoMaterialModule,
 
 
   ],
